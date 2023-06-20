05c6a116b060f9e4ac51d6d454add39a3ee54a34
suynnn <suy00065@gmail.com>
2023-06-20 22:58:11 +0900

    [BOJ] 9095번 - 1, 2, 3 더하기 / 실버 3

---
+++ src/main/java/algorithm/baekjoon/dp/Plus123_9095.java
@@ -0,0 +1,29 @@
+package algorithm.baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Plus123_9095 {
+
+    static Integer[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            dp = new Integer[11];
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int j = 4; j <= n; j++) {
+                dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+            }
+
+            System.out.println(dp[n]);
+        }
+    }
+}
