e52e27266070d9ee9111291fb99586caefb41608
suynnn <suy00065@gmail.com>
2023-07-08 15:10:52 +0900

    [BOJ] 1475번 - 방 번호 / 실버 5

---
+++ src/main/java/algorithm/baekjoon/implementation/RoomNumber_BOJ1475.java
@@ -0,0 +1,33 @@
+package algorithm.baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RoomNumber_BOJ1475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = Integer.parseInt(bf.readLine());
+        int[] nums = new int[10];
+
+        while (sum > 0) {
+            nums[sum % 10]++;
+            sum /= 10;
+        }
+
+        int max = 1;
+        for (int i = 0; i < 10; i++) {
+            if((i == 6 || i == 9)) {
+                continue;
+            }
+            if (max < nums[i]) {
+                max = nums[i];
+            }
+        }
+        if (max < ((nums[6] + nums[9] + 1) /2))
+            max = (nums[6] + nums[9] + 1) /2;
+
+        System.out.println(max);
+    }
+}
